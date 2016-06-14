719cecc4c3670c4f53a283519c05df842d51b90c
Inthrayuth Mahaphol and Nick Lyman <inthrayuth@>
2016-06-14 14:16:06 -0700

    Modify model to seperate House and Senate with two API calls and returns. Link subcommittee by committee parent ID. Next, we will make subcommittee link work to display subcommittees within each committee.

--- app/routes/committees.js
+++ app/routes/committees.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
+  var urlHouse = 'http://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=false&apikey=7a6b1445457841dc853711413517cbc0';
+
+  var urlSenate = 'http://congress.api.sunlightfoundation.com/committees?chamber=senate&subcommittee=false&apikey=7a6b1445457841dc853711413517cbc0';
 
 export default Ember.Route.extend({
-  model: function() {
-    var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=7a6b1445457841dc853711413517cbc0';
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.results;
+  model() {
+    return Ember.RSVP.hash({
+      house: Ember.$.getJSON(urlHouse).then(function(responseJSON) {
+        return responseJSON.results;
+      }),
+      senate: Ember.$.getJSON(urlSenate).then(function(responseJSON) {
+        return responseJSON.results;
+      }),
     });
-  }
+  },
 });
